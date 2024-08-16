8b1f84bec697588fef2b91daaad8521da62da8c1
EscapeFromJava <reske.artur@gmail.com>
2024-08-16 19:38:53 +0300

    Рефакторинг

--- src/main/java/com/reske/taptapbot/model/QuestionnaireItem.java
+++ src/main/java/com/reske/taptapbot/model/QuestionnaireItem.java
@@ -24,13 +24,4 @@ public class QuestionnaireItem {
     private boolean used;
     private boolean last;
 
-    public QuestionnaireItem(String question, String answer, String option1, String option2, String option3, String option4) {
-        this.question = question;
-        this.answer = answer;
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
-        this.option4 = option4;
-    }
-
 }
