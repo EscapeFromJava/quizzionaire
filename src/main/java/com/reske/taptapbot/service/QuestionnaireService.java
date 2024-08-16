0474a4105defd6476746e2eb2a32645ef05a7a90
EscapeFromJava <reske.artur@gmail.com>
2024-08-16 16:33:12 +0300

    Добавлен конфиг liquibase

--- src/main/java/com/reske/taptapbot/service/QuestionnaireService.java
+++ src/main/java/com/reske/taptapbot/service/QuestionnaireService.java
@@ -2,7 +2,6 @@ package com.reske.taptapbot.service;
 
 import com.reske.taptapbot.model.QuestionnaireItem;
 import com.reske.taptapbot.repository.QuestionnaireItemRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,24 +15,6 @@ public class QuestionnaireService {
     private static final Random RANDOM = new Random();
 
     private final QuestionnaireItemRepository repository;
-
-    @PostConstruct
-    public void init() {
-        QuestionnaireItem q1 = new QuestionnaireItem(
-                "Как называется еврейский Новый год?",
-                "Рош ха-Шана",
-                "Ханука", "Йом Кипур", "Кванза", "Рош ха-Шана"
-        );
-        QuestionnaireItem q2 = new QuestionnaireItem(
-                "Сколько синих полос на флаге США?",
-                "0",
-                "6", "7", "13", "0");
-        QuestionnaireItem q3 = new QuestionnaireItem(
-                "Кто из этих персонажей не дружит с Гарри Поттером?",
-                "Драко Малфой",
-                "Рон Уизли", "Невилл Лонгботтом", "Драко Малфой", "Гермиона Грейнджер");
-        repository.saveAll(List.of(q1, q2, q3));
-    }
 
     public QuestionnaireItem getRandomItem() {
         List<QuestionnaireItem> items = repository.findByUsed(false);
@@ -62,7 +43,6 @@ public class QuestionnaireService {
 
     public void restart() {
         repository.deleteAll();
-        init();
     }
 
 }
