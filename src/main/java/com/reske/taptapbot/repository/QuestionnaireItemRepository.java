8b264877834440566d51ecaac6668c0ef40a10b1
EscapeFromJava <reske.artur@gmail.com>
2024-08-16 19:25:54 +0300

    Добавлен тестовая конфигруация на сброс вопросов во время старта приложения

--- src/main/java/com/reske/taptapbot/repository/QuestionnaireItemRepository.java
+++ src/main/java/com/reske/taptapbot/repository/QuestionnaireItemRepository.java
@@ -2,11 +2,19 @@ package com.reske.taptapbot.repository;
 
 import com.reske.taptapbot.model.QuestionnaireItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface QuestionnaireItemRepository extends JpaRepository<QuestionnaireItem, Integer> {
 
     List<QuestionnaireItem> findByUsed(boolean used);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE QuestionnaireItem q SET q.used = FALSE, q.last = FALSE")
+    void restart();
 
 }
